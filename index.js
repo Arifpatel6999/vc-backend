f455cdfc98cd8d3e8b769e50fdb47b18b905e8bc
shahzad-eth <rizvishahzad44@gmail.com>
2025-05-24 23:02:44 +0530

    v-backend

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 // using middleware
 app.use(express.json())
 app.use(cors({
-    origin: ["https://vc-backend-1hox.onrender.com", "http://localhost:5173"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"]
 }))
 
